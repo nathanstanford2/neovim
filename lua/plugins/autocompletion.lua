8b610f96421108f0d3e9e4f030f6ce5c23962845
Nathan Stanford <nathan@projectadmission.com>
2025-10-07 22:05:59 -0400

    adding vscode loaders

--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -10,7 +10,10 @@ return {
     config = function()
       local cmp = require("cmp")
       local luasnip = require("luasnip")
-
+      require("luasnip.loaders.from_vscode").lazy_load({
+        paths = { vim.fn.stdpath('config') .. '/snippets' },
+        exclude = { 'tex', 'plaintex' } -- Exclude snippets for these filetypes
+      })
       cmp.setup({
         snippet = {
           expand = function(args)
