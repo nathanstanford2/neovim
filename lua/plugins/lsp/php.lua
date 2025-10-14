e609f9cbd26661e0ddacaef2f835449491933c51
Nathan Stanford <nathan@projectadmission.com>
2025-10-13 21:35:25 -0400

    Fix Vue plugin and add glance

--- lua/plugins/lsp/php.lua
+++ lua/plugins/lsp/php.lua
@@ -1,16 +1,16 @@
 -- lua/plugins/lsp/php.lua
 return {
-  {
-    "neovim/nvim-lspconfig",
-    dependencies = { "williamboman/mason-lspconfig.nvim" },
-    config = function():
-      vim.lsp.config('intelephense', {
-          settings = {
-              intelephense = {
-                  files = { maxSize = 5000000 },
-              },
-          },
-      })
-    end,
-  },
+    {
+        "neovim/nvim-lspconfig",
+        dependencies = { "williamboman/mason-lspconfig.nvim" },
+        config = function()
+            vim.lsp.config('intelephense', {
+                settings = {
+                    intelephense = {
+                        files = { maxSize = 5000000 },
+                    },
+                },
+            })
+        end,
+    },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,6 +1,5 @@
 return {
     'neovim/nvim-lspconfig',
-
     dependencies = {
         'williamboman/mason.nvim',
         'williamboman/mason-lspconfig.nvim',
@@ -11,12 +10,10 @@ return {
         'hrsh7th/nvim-cmp',
         'j-hui/fidget.nvim',
     },
-
     config = function()
         local cmp = require('cmp')
         local cmp_lsp = require('cmp_nvim_lsp')
 
-        -- LSP capabilities for nvim-cmp completion
         local capabilities = vim.tbl_deep_extend(
             'force',
             {},
@@ -31,107 +28,36 @@ return {
             ensure_installed = {
                 'lua_ls',
                 'tailwindcss',
-                'vue_ls',
                 'vtsls',
                 'intelephense',
             }
         })
 
-        vim.lsp.config('*', { capabilities = capabilities })
+        local vue_plugin = {
+            name = '@vue/typescript-plugin',
+            location = vim.fn.stdpath('data') ..
+                '/mason/packages/vue-language-server/node_modules/@vue/language-server',
+            languages = { 'vue' },
+            configNamespace = 'typescript',
+        }
 
-        -------------------------------------------------------------------------
-        -- LSP Configurati path aliases
-        -------------------------------------------------------------------------
-        local function resolve_path_alias(import_path)
-            if import_path:match("^@/") then
-                local relative_path = import_path:gsub("^@/", "assets/")
-                return relative_path
-            end
-            return import_path
-        end
-
-        -------------------------------------------------------------------------
-        -- Helper: go to definition with alias handling
-        -------------------------------------------------------------------------
-        local function goto_definition_with_alias()
-            local params = vim.lsp.util.make_position_params()
-            vim.lsp.buf_request(0, 'textDocument/definition', params, function(err, result)
-                if result and not vim.tbl_isempty(result) then
-                    vim.lsp.util.jump_to_location(result[1], 'utf-8')
-                else
-                    local line = vim.api.nvim_get_current_line()
-                    local import_match = line:match("from%s+['\"]([^'\"]+)['\"]") or line:match("import%s+['\"]([^'\"]+)['\"]")
-                    if import_match then
-                        local resolved = resolve_path_alias(import_match)
-                        local root = vim.fs.root(0, { 'package.json', '.git' })
-                        if root then
-                            local full_path = root .. '/' .. resolved
-                            if not full_path:match("%.vue$") and not full_path:match("%.ts$") and not full_path:match("%.js$") then
-                                full_path = full_path .. '.vue'
-                            end
-                            if vim.fn.filereadable(full_path) == 1 then
-                                vim.cmd('edit ' .. full_path)
-                            else
-                                vim.notify("Could not find file: " .. full_path, vim.log.levels.WARN)
-                            end
-                        end
-                    else
-                        vim.notify("No definition found", vim.log.levels.WARN)
-                    end
-                end
-            end)
-        end
-
-        -------------------------------------------------------------------------
-        -- LSP Configurations
-        -------------------------------------------------------------------------
         vim.lsp.config('vtsls', {
-            root_markers = { 'package.json', 'tsconfig.json', '.git' },
-            init_options = {
-                plugins = {
-                    {
-                        name = '@vue/typescript-plugin',
-                        location = vim.fn.expand("$MASON/packages/vue-language-server/node_modules/@vue/language-server"),
-                        languages = { 'vue' },
-                    },
-                },
-            },
             settings = {
                 vtsls = {
-                    autoUseWorkspaceTsdk = true,
-                    experimental = {
-                        completion = { enableServerSideFuzzyMatch = true },
-                    },
-                },
-                typescript = {
-                    tsdk = vim.fn.expand("$MASON/packages/typescript-language-server/node_modules/typescript/lib"),
-                    updateImportsOnFileMove = { enabled = "always" },
-                    suggest = { completeFunctionCalls = true },
-                    inlayHints = {
-                        parameterNames = { enabled = "literals" },
-                        parameterTypes = { enabled = true },
-                        variableTypes = { enabled = true },
-                        propertyDeclarationTypes = { enabled = true },
-                        functionLikeReturnTypes = { enabled = true },
-                        enumMemberValues = { enabled = true },
+                    tsserver = {
+                        globalPlugins = {
+                            vue_plugin,
+                        },
                     },
                 },
             },
             filetypes = { 'typescript', 'javascript', 'javascriptreact', 'typescriptreact', 'vue' },
         })
 
-        vim.lsp.config('vue_ls', {
-            root_markers = { 'package.json', 'vue.config.js', 'vite.config.ts' },
-            init_options = {
-                vue = {
-                    hybridMode = true,
-                },
-            },
-        })
+        -- Configure other LSPs with default capabilities
+        vim.lsp.config('*', { capabilities = capabilities })
 
-        -------------------------------------------------------------------------
-        -- CMP setup
-        -------------------------------------------------------------------------
+        -- Setup completion
         local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
         cmp.setup({
@@ -148,9 +74,6 @@ return {
             }),
         })
 
-        -------------------------------------------------------------------------
-        -- Diagnostics
-        -------------------------------------------------------------------------
         vim.diagnostic.config({
             virtual_text = true,
             update_in_insert = true,
@@ -158,44 +81,25 @@ return {
                 focusable = true,
                 style = 'minimal',
                 border = 'rounded',
-                source = 'always',
+                source = true,
                 header = '',
                 prefix = '',
             },
         })
 
-        -------------------------------------------------------------------------
-        -- Autocmd: set keymaps after LSP attaches
-        -------------------------------------------------------------------------
         vim.api.nvim_create_autocmd('LspAttach', {
             callback = function(args)
                 local bufnr = args.buf
                 local client = vim.lsp.get_client_by_id(args.data.client_id)
                 if not client then return end
 
-                local map = function(keys, func, desc)
-                    if func then
-                        vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
-                    end
-                end
+                local opts = { buffer = bufnr }
 
-                if client.server_capabilities.definitionProvider then
-                    if client.name == 'vtsls' or client.name == 'vue_ls' then
-                        map('gd', goto_definition_with_alias, 'Go to Definition')
-                    else
-                        map('gd', vim.lsp.buf.definition, 'Go to Definition')
-                    end
-                end
-
-                if client.server_capabilities.referencesProvider then
-                    map('gr', vim.lsp.buf.references, 'Go to References')
-                end
-                if client.server_capabilities.hoverProvider then
-                    map('K', vim.lsp.buf.hover, 'Hover Documentation')
-                end
-                if client.server_capabilities.renameProvider then
-                    map('<leader>rn', vim.lsp.buf.rename, 'Rename Symbol')
-                end
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+                vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
             end,
         })
     end,
