9d2b4fd2246f7b86f2ee3386a56c7548eac4b4df
Nathan Stanford <nathan@projectadmission.com>
2025-10-21 21:26:53 -0400

    feat(lsp): Add PHP language server to Mason configuration
    - Add intelephense (PHP language server) to ensure_installed list

--- lua/nathan/plugins/lsp/mason.lua
+++ lua/nathan/plugins/lsp/mason.lua
@@ -1,46 +1,48 @@
 return {
-  {
-    "williamboman/mason-lspconfig.nvim",
-    opts = {
-      -- list of servers for mason to install
-      ensure_installed = {
-        "vtsls",
-        "html",
-        "cssls",
-        "tailwindcss",
-        "svelte",
-        "lua_ls",
-        "emmet_ls",
-        "eslint",
-      },
-    },
-    dependencies = {
-      {
-        "williamboman/mason.nvim",
+    {
+        "williamboman/mason-lspconfig.nvim",
         opts = {
-          ui = {
-            icons = {
-              package_installed = "✓",
-              package_pending = "➜",
-              package_uninstalled = "✗",
+            -- list of servers for mason to install
+            ensure_installed = {
+                "vtsls",
+                "html",
+                "cssls",
+                "tailwindcss",
+                "intelephense",
+                "svelte",
+                "lua_ls",
+                "emmet_ls",
+                "eslint",
             },
-          },
         },
-      },
-      "neovim/nvim-lspconfig",
+        dependencies = {
+            {
+                "williamboman/mason.nvim",
+                opts = {
+                    ui = {
+                        icons = {
+                            package_installed = "✓",
+                            package_pending = "➜",
+                            package_uninstalled = "✗",
+                        },
+                    },
+                },
+            },
+            "neovim/nvim-lspconfig",
+        },
     },
-  },
-  {
-    "WhoIsSethDaniel/mason-tool-installer.nvim",
-    opts = {
-      ensure_installed = {
-        "prettier", -- prettier formatter
-        "stylua", -- lua formatter
-        "eslint_d",
-      },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        opts = {
+            ensure_installed = {
+                "prettier", -- prettier formatter
+                "stylua", -- lua formatter
+                "eslint_d",
+            },
+        },
+        dependencies = {
+            "williamboman/mason.nvim",
+        },
     },
-    dependencies = {
-      "williamboman/mason.nvim",
-    },
-  },
 }
+
