63c9d9797d717b5de158cb4d30c3931e4e3da643
Nathan <nathan@delawareinc.com>
2025-12-10 16:48:09 -0500

    swapping which-key layout

--- lua/nathan/plugins/which-key.lua
+++ lua/nathan/plugins/which-key.lua
@@ -3,7 +3,7 @@ return {
     event = "VeryLazy",
     opts_extend = { "spec" },
     opts = {
-        preset = "helix",
+        preset = "modern",
         defaults = {},
         spec = {
             {
